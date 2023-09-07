ad9b86da4e7d02bedf07a27a8bdabbe159888a4b
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2023-09-07 17:39:56 -0300

    feat: add a print function direct in the asm code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     fmt::{self, Display, Formatter},
     io::BufWriter,
-    process::exit,
+    process::{self, exit, Command},
 };
 
 // TODO: create assembly macro for writing
@@ -127,33 +127,83 @@ struct Compiler {
 }
 
 impl Compiler {
-    fn compile(&mut self, writer: &mut impl std::io::Write) {
-        writeln!(writer, "segment .text").unwrap();
-        writeln!(writer, "global _start").unwrap();
-        writeln!(writer, "_start:").unwrap();
+    fn compile(&mut self, writer: &mut impl std::io::Write) -> Result<(), std::io::Error> {
+        writeln!(writer, "segment .text")?;
+        writeln!(writer, "global _start")?;
+        writeln!(writer, "_start:")?;
 
         while let Some(token) = self.program.pop() {
             match token {
                 Token::Integer(_, n) => {
-                    twriteln!(writer, ";; Push Integer").unwrap();
-                    twriteln!(writer, "mov rax, {}", n).unwrap();
-                    twriteln!(writer, "push rax").unwrap();
+                    twriteln!(writer, ";; Push Integer")?;
+                    twriteln!(writer, "mov rax, {}", n)?;
+                    twriteln!(writer, "push rax")?;
                 }
-                Token::Print(_) => todo!(),
                 Token::Plus(_) => {
-                    twriteln!(writer, ";; Plus").unwrap();
-                    twriteln!(writer, "pop rax").unwrap();
-                    twriteln!(writer, "pop rbx").unwrap();
-                    twriteln!(writer, "add rax, rbx").unwrap();
-                    twriteln!(writer, "push rax").unwrap();
+                    twriteln!(writer, ";; Plus")?;
+                    twriteln!(writer, "pop rax")?;
+                    twriteln!(writer, "pop rbx")?;
+                    twriteln!(writer, "add rax, rbx")?;
+                    twriteln!(writer, "push rax")?;
+                }
+                Token::Print(_) => {
+                    twriteln!(writer, ";; Print")?;
+                    twriteln!(writer, "pop rdi")?;
+                    twriteln!(writer, "call print64")?;
                 }
             }
         }
 
-        twriteln!(writer, ";; Exit").unwrap();
-        twriteln!(writer, "mov rax, 60").unwrap();
-        twriteln!(writer, "mov rdi, 0").unwrap();
-        twriteln!(writer, "syscall").unwrap();
+        // TODO: remove this when print is implemented
+
+        twriteln!(writer, ";; Exit")?;
+        twriteln!(writer, "mov rax, 60")?;
+        twriteln!(writer, "mov rdi, 0")?;
+        twriteln!(writer, "syscall")?;
+
+        writeln!(writer, "print64:")?;
+        twriteln!(writer, "sub     rsp, 40")?;
+        twriteln!(writer, "mov     BYTE [rsp+31], 10")?;
+        twriteln!(writer, "test    rdi, rdi")?;
+        twriteln!(writer, "je      .L4")?;
+        twriteln!(writer, "mov     rsi, 7378697629483820647")?;
+        twriteln!(writer, "mov     ecx, 29")?;
+
+        writeln!(writer, ".L3:")?;
+        twriteln!(writer, "mov     rax, rdi")?;
+        twriteln!(writer, "imul    rsi")?;
+        twriteln!(writer, "mov     rax, rdi")?;
+        twriteln!(writer, "sar     rax, 63")?;
+        twriteln!(writer, "sar     rdx, 2")?;
+        twriteln!(writer, "sub     rdx, rax")?;
+        twriteln!(writer, "lea     rax, [rdx+rdx*4]")?;
+        twriteln!(writer, "add     rax, rax")?;
+        twriteln!(writer, "sub     rdi, rax")?;
+        twriteln!(writer, "mov     rax, rcx")?;
+        twriteln!(writer, "add     edi, 48")?;
+        twriteln!(writer, "mov     BYTE [rsp+1+rcx], dil")?;
+        twriteln!(writer, "mov     rdi, rdx")?;
+        twriteln!(writer, "sub     rcx, 1")?;
+        twriteln!(writer, "test    rdx, rdx")?;
+        twriteln!(writer, "jne     .L3")?;
+        twriteln!(writer, "cdqe")?;
+        twriteln!(writer, "mov     edx, 32")?;
+        twriteln!(writer, "sub     rdx, rax")?;
+        writeln!(writer, ".L2:")?;
+        twriteln!(writer, "lea     rsi, [rsp+rax]")?;
+        twriteln!(writer, "mov     edi, 1")?;
+        twriteln!(writer, "mov     eax, 1")?;
+        twriteln!(writer, "syscall")?;
+        twriteln!(writer, "add     rsp, 40")?;
+        twriteln!(writer, "ret")?;
+        writeln!(writer, ".L4:")?;
+        twriteln!(writer, "mov     edx, 2")?;
+        twriteln!(writer, "mov     eax, 30")?;
+        twriteln!(writer, "jmp     .L2")?;
+
+        writer.flush()?;
+
+        Ok(())
     }
 }
 
@@ -212,7 +262,14 @@ fn main() {
         usage(&args[0], None);
     }
 
-    let buffer = std::fs::read_to_string(&args[2]).unwrap();
+    let buffer = match std::fs::read_to_string(&args[2]) {
+        Ok(buffer) => buffer,
+        Err(err) => {
+            println!("Error: {}", err);
+            process::exit(1);
+        }
+    };
+
     let buffer = buffer.chars().collect::<Vec<char>>();
 
     let lexer = Lexer::new(&buffer);
@@ -228,10 +285,34 @@ fn main() {
                 usage(&args[0], Some(KroksMode::Compile));
             }
 
-            let mut output = std::fs::File::create(&args[3]).unwrap();
+            let asm_filename = format!("{}.asm", args[3]);
+            let mut output = std::fs::File::create(&asm_filename).unwrap();
             let mut output = BufWriter::new(&mut output);
 
-            compiler.compile(&mut output);
+            match compiler.compile(&mut output) {
+                Ok(_) => {
+                    Command::new("nasm")
+                        .arg("-f")
+                        .arg("elf64")
+                        .arg("-ggdb")
+                        .arg("-o")
+                        .arg(&format!("{}.o", args[3]))
+                        .arg(&asm_filename)
+                        .output()
+                        .expect("failed to execute process");
+
+                    Command::new("ld")
+                        .arg("-o")
+                        .arg(&args[3])
+                        .arg(&format!("{}.o", args[3]))
+                        .output()
+                        .expect("failed to execute process");
+                }
+                Err(err) => {
+                    println!("Error: {}", err);
+                    process::exit(1);
+                }
+            }
         }
         "sim" => {
             let mut simulator = Simulator { program: stack };
